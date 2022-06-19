d3d15327e74e246d410a9c0001a4df0f37d9733c
Mauriciiow <mauricioliveira639@gmail.com>
2022-06-19 14:28:10 -0300

    inicio

---
+++ src/main/java/br/com/springboot/model/User.java
@@ -0,0 +1,28 @@
+package br.com.springboot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+
+}
---
+++ src/main/java/br/com/springboot/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package br.com.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import br.com.springboot.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    // @Query("SELECT u from User u where u.id > :id")
+    // public List<User> findAllMoreThan(@Param("id") Long id); testando sem palavras reservada
+    
+    public List<User> findByIdGreaterThan(Long id);
+
+    public List<User> findByNameIgnoreCase(String name);
+    
+}
