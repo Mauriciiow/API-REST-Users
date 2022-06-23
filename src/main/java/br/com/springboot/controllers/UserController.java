1d6bf562453570f3da2a90087819251e5e5c4224
Mauriciiow <mauricioliveira639@gmail.com>
2022-06-23 15:38:40 -0300

    usando o service direto no lugar do repositorio direto

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -3,8 +3,16 @@ package br.com.springboot.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,66 +22,96 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.dtos.UserDto;
 import br.com.springboot.model.User;
 import br.com.springboot.repository.UserRepository;
+import br.com.springboot.services.UserService;
 import lombok.var;
 
 @RestController
+@CrossOrigin(origins = "x", maxAge = 3600)
 @RequestMapping("/")
 
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping("/users/{id}")
-    public User user(@PathVariable("id") Long id){
-
-        Optional<User> userFind = this.userRepository.findById(id);
-
-        if (userFind.isPresent()) {
-            return userFind.get();
-        }
-
-        return null;
-
-    }
+    private UserService userService;
 
     @PostMapping("/add_users")
-    public User addUser(@RequestBody User user){
-        return this.userRepository.save(user);
+    public ResponseEntity<Object> addUser(@RequestBody @Valid UserDto user){
+
+        if (userService.existsByEmail(user.getEmail())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Email is alredy in use");
+        }
+
+        var userModel = new User();
+        BeanUtils.copyProperties(user, userModel);
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
        
     }
 
     @GetMapping("/users")
-    public List<User> list() {
-        return this.userRepository.findAll();
+    public ResponseEntity<List<User>> list() {
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll());
     }
 
-    @GetMapping("/users_more/{id}")
-    public List<User> listMoreThan(@PathVariable("id") Long id) {
-        return this.userRepository.findByIdGreaterThan(id);
-    }
+      @GetMapping("/users/{id}")
+        ResponseEntity<Object> user(@PathVariable("id") Long id){
 
-    @GetMapping("/users_name/{name}")
-    public List<User> listByName(@PathVariable("name") String name) {
-        return this.userRepository.findByNameIgnoreCase(name);
-    }
+        var userFind = userService.findById(id);
 
-    @PatchMapping("/edit_user/{id}")
-    public User updateUser(@PathVariable("id") Long id, @RequestBody User user) {
-
-        Optional<User> userExist = this.userRepository.findById(id);
-
-        if (!userExist.isPresent()) {
-            return null;
+        if (userFind.isPresent()) {
+            return ResponseEntity.status(HttpStatus.FOUND).body(userFind.get());
         }
 
-        User userUpdate = userExist.get();
-        userUpdate.setName(user.getName());
-        userUpdate.setUsername(user.getUsername());
-        userRepository.save(userUpdate);
-        return userUpdate;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+
+    }
+
+    // @GetMapping("/users_more/{id}")
+    // public List<User> listMoreThan(@PathVariable("id") Long id) {
+    //     return this.userRepository.findByIdGreaterThan(id);
+    // }
+
+    // @GetMapping("/users_name/{name}")
+    // public List<User> listByName(@PathVariable("name") String name) {
+    //     return this.userRepository.findByNameIgnoreCase(name);
+    // }
+
+    @PatchMapping("/edit_user/{id}")
+    public ResponseEntity<Object> updateUser(@PathVariable("id") Long id, @RequestBody User user) {
+
+        var userExist = userService.findById(id);
+
+        if (!userExist.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        var newUser = userExist.get();
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+        newUser.setTelefone(user.getTelefone());
+        
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(userService.save(newUser));
+
        
     }
+
+    @DeleteMapping("/delete_user/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable("id") Long id){
+        var userExist = userService.findById(id);
+
+        if (!userExist.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        userService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Deleted");
+        
+
+        
+    }
 }
+
+
